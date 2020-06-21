d12e9f4b7e5f235f96601a695013f90ee97ed413
ttnaoto <ttnaoto@gmail.com>
2020-06-21 11:10:30 +0900

    add:darkModeを実装

--- app/src/main/java/com/example/storyadvancer/MainActivity.kt
+++ app/src/main/java/com/example/storyadvancer/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.storyadvancer
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.storyadvancer.repository.Repository
 import com.example.storyadvancer.ui.worldlist.WorldListFragment
 
@@ -18,5 +21,30 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.main_container, WorldListFragment())
                 .commitNow()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.night_mode, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+            R.id.night_mode -> {
+                changeTheme()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+    }
+
+    private fun changeTheme() {
+        val mode = when (AppCompatDelegate.getDefaultNightMode()) {
+            AppCompatDelegate.MODE_NIGHT_YES -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+            else -> AppCompatDelegate.MODE_NIGHT_YES
+        }
+        AppCompatDelegate.setDefaultNightMode(mode)
     }
 }
